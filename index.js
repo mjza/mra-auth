05d86977ad5359180538394b33dea4f383e8babf
mahdi.ansari@reportcycle.com <mahdi.ansari@reportcycle.com>
2023-11-27 02:20:44 -0700

    Swagger was addaed. Validation was added.

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`
 });
+
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const db = require('./database'); // Your database connection logic
+const basicAuth = require('express-basic-auth');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 
 const app = express();
-app.use(express.json()); // Middleware for parsing JSON
+
+// Middleware for parsing JSON
+app.use(express.json());
 
 // Import routes
 const authRoutes = require('./routes/authRoutes');
-
 // Use routes
-app.use('/auth', authRoutes);
+app.use('/', authRoutes);
+
+// Swagger definition
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express Auth API',
+    version: '1.0.0',
+    description: 'A simple CRUD Auth API application',
+  },
+  servers: [
+    {
+      url: process.env.BASE_URL || 'http://localhost:3000',
+      description: 'Development server',
+    },
+  ],
+};
+
+// Options for the swagger docs
+const options = {
+  swaggerDefinition,
+  // Paths to files containing Swagger annotations
+  apis: ['./routes/*.js'],
+};
+
+// Initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
+// Basic auth credentials for accessing Swaggar
+const users = {
+  'admin': 'mjzA626$'
+};
+
+// Use swaggerUi to serve swagger docs
+app.use('/api-docs', basicAuth({
+  users: users,
+  challenge: true // Causes browsers to show a login dialog
+}), swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 const db = require('../database');
 
@@ -7,9 +8,52 @@ const router = express.Router();
 // Import controllers if you have them
 // const authController = require('../controllers/authController');
 
-// Registration logic here
-router.post('/register', async (req, res) => {
-  
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     description: Create a new user in the system.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User successfully registered
+ *       400:
+ *         description: Invalid input
+ *       500:
+ *         description: Server error
+ */
+router.post('/register', [
+  body('username').isLength({ min: 5, max: 30 }).withMessage('Username must be between 5 and 30 characters.'),
+  body('email').isEmail().withMessage('Invalid email address.'),
+  body('email').isLength({ min: 5, max: 255 }).withMessage('Email must be between 5 and 255 characters.'),
+  body('password').isLength({ min: 8, max: 30 }).withMessage('Password must be between 8 and 30 characters.'),
+  body('password').matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter'),
+  body('password').matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter'),
+  body('password').matches(/\d/).withMessage('Password must contain at least one digit'),
+  body('password').matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage('Password must contain at least one symbol')
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   try {
         const { username, email, password } = req.body;
 
@@ -31,10 +75,18 @@ router.post('/register', async (req, res) => {
   }
 });
 
-// Login logic here
-router.post('/login', (req, res) => {
+// Login route
+router.post('/login', [
+  body('email').isEmail().withMessage('Invalid email address.'),
+  body('password').exists().withMessage('Password is required.')
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
 });
+
 
 
 
