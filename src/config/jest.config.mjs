34dce39011cf115a0861b8e7784fbb3b626b67ae
Mahdi Ansari <mahdi.ansari1@ucalgary.ca>
2024-09-30 17:22:46 -0600

    reduced the timeout for tests

--- src/config/jest.config.mjs
+++ src/config/jest.config.mjs
@@ -1,6 +1,6 @@
 export default {
     rootDir: '../../',
-    testTimeout: 200000,
+    testTimeout: 5000,
     globalSetup: '<rootDir>/src/config/jest.globalSetup.mjs',
     globalTeardown: '<rootDir>/src/config/jest.globalTeardown.mjs',
     setupFilesAfterEnv: ['<rootDir>/src/config/jest.setup.mjs'],
