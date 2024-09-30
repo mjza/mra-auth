40fea89b78cc859ac2cdef1e92bf8682dbec4735
Mahdi Ansari <mahdi.ansari1@ucalgary.ca>
2024-09-30 17:28:11 -0600

    Increased timeout

--- src/config/jest.config.mjs
+++ src/config/jest.config.mjs
@@ -1,6 +1,6 @@
 export default {
     rootDir: '../../',
-    testTimeout: 10000,
+    testTimeout: 100000,
     globalSetup: '<rootDir>/src/config/jest.globalSetup.mjs',
     globalTeardown: '<rootDir>/src/config/jest.globalTeardown.mjs',
     setupFilesAfterEnv: ['<rootDir>/src/config/jest.setup.mjs'],
