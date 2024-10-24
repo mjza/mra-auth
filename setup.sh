718a634aeecab28b8162953747ca13420a676bf5
Mahdi Ansari <mahdi.ansari1@ucalgary.ca>
2024-10-23 20:11:47 -0600

    [Git] Add a hook for commits
    Add a hook and a message template to organize commits

--- setup.sh
+++ setup.sh
@@ -3,4 +3,8 @@
 # Configure Git to use the commit message template locally
 git config --local commit.template .gitmessage.txt
 
-# Optionally, set up other local configurations or hooks
+# Configure Git to use the custom hooks directory
+git config --local core.hooksPath .githooks
+
+# Make sure all hook scripts are executable
+chmod +x .githooks/*
