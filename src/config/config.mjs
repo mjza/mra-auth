691c4a4ee10d9804660a1ae616874776c24f5532
Mahdi Ansari <mahdi@reportcycle.com>
2024-10-25 16:59:57 -0600

    [Update] Change the way we determine the path to .env file
    In the past we used to use dotenv, now we use dotenv-cli, now it does not run on tests anymore

--- src/config/config.mjs
+++ src/config/config.mjs
@@ -1,12 +1,5 @@
-// set the address for .env file 
-import dotenv from 'dotenv';
 import { utils } from '@reportcycle/mra-utils';
-// set the env addresses
-dotenv.config({
-  path: 'src/config/.env'
-});
 
-// config utils
 utils.config({
     secretKey: process.env.SECRET_KEY,
     developmentToken: process.env.X_DEVELOPMENT_TOKEN,
